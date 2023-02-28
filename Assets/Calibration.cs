a32b496ced8b6240abfa9470f948ece606fad1d4
QuillJoy <jwquill81@gmail.com>
2023-02-27 20:19:04 -0800

    Clean up Calibration script

--- Assets/Calibration.cs
+++ Assets/Calibration.cs
@@ -2,6 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/**********************************/
+/*       Calibration Script
+ * ********************************
+ * This script will ask the User to
+ * do two actions:
+ *   1. Walk 1 meter in the x 
+ *   direction.
+ *   
+ *   2. Look 90 degrees to 
+ *   the right.
+ *   
+ * The script will then retrieve 
+ * data from the telemetry stream
+ * to verify that VISION is working.
+ * 
+ * Any line with "Debug.Log" may be
+ * replaced with Isaac's display
+ * solution
+ * 
+ * 
+ * 
+ * 
+/*                               */
+/**********************************/
 public class Calibration : MonoBehaviour
 {
     Vector3 initialPosition;
@@ -10,19 +34,14 @@ public class Calibration : MonoBehaviour
     float currentAngleY;
     void Start()
     {
-        initialPosition = FindObjectOfType<TelemetryStream>().getInitialPosition();
-        initialAngleY = FindObjectOfType<TelemetryStream>().getInitialAngleY();
+        // Add the Instruction functions as events in 
+        // the EventManager
         EventManager.CalibrationEvent1 += Instruction1;
         EventManager.CalibrationEvent2 += Instruction2;
     }
 
-    private void Update()
-    {
-        currentPosition = FindObjectOfType<TelemetryStream>().getCurrentPosition();
-        currentAngleY = FindObjectOfType<TelemetryStream>().getCurrentAngleY();
-    }
-
-
+    // Each instruction will display instructions on
+    // the screen and then execute data verification.
     private void Instruction1()
     {
         StartCoroutine(showInstructions1());
@@ -35,12 +54,15 @@ public class Calibration : MonoBehaviour
         StartCoroutine(calibration2());
     }
 
+    // calibration1 and calibration2 
     IEnumerator calibration1()
     {
+        initialPosition = FindObjectOfType<TelemetryStream>().getCurrentPosition();
         Vector3 goalPosition = initialPosition + new Vector3(1.0f, 0.5f, 0.0f);
-        initialPosition = FindObjectOfType<TelemetryStream>().getInitialPosition();
+        currentPosition.x = FindObjectOfType<TelemetryStream>().getCurrentPosition().x;
         while (currentPosition.x < goalPosition.x)
         {
+            currentPosition.x = FindObjectOfType<TelemetryStream>().getCurrentPosition().x;
             yield return null;
         }
         Debug.Log("One meter walked. Thank you!");
@@ -50,7 +72,9 @@ public class Calibration : MonoBehaviour
 
     IEnumerator calibration2()
     {
+        initialAngleY = FindObjectOfType<TelemetryStream>().getCurrentAngleY();
         float goalAngle = initialAngleY + (90);
+        currentAngleY = FindObjectOfType<TelemetryStream>().getCurrentAngleY();
         while (currentAngleY < goalAngle)
         {
             yield return null;
