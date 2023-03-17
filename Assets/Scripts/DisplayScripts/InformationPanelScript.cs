3d83c4abbf5913c1f2f47efdd0c4d1f55015757e
QuillJoy <jwquill81@gmail.com>
2023-03-17 15:36:57 -0700

    Add script to telemetry stream button and move connect button

--- Assets/Scripts/DisplayScripts/InformationPanelScript.cs
+++ Assets/Scripts/DisplayScripts/InformationPanelScript.cs
@@ -6,27 +6,34 @@ public class InformationPanelScript : MonoBehaviour
 {
     public GameObject MOPanel;
     public GameObject spectroPanel;
+    public GameObject telemPanel;
 
 
     public bool isShowingMO;
     public bool isShowingSpectro;
+    public bool isShowingTelem;
     // Start is called before the first frame update
     void Start()
     {
         isShowingMO = false;
         isShowingSpectro = false;
+        isShowingTelem = false;
         MOPanel.SetActive(false);
         spectroPanel.SetActive(false);
+        telemPanel.SetActive(false);
+
     }
 
     public void OnButtonPressMO()
     {
         isShowingMO = !isShowingMO;
-        if(isShowingSpectro == true)
+        if(isShowingSpectro == true || isShowingTelem == true)
         {
             isShowingSpectro = false;
+            isShowingTelem = false;
             spectroPanel.SetActive(isShowingSpectro);
             MOPanel.SetActive(isShowingMO);
+            telemPanel.SetActive(isShowingTelem);
             foreach (KeyValuePair<GameObject, bool> item in FindObjectOfType<MissionObjectivesDataHolder>().GetMissionObjectives())
             {
                 if (item.Value == true)
@@ -50,11 +57,13 @@ public class InformationPanelScript : MonoBehaviour
     public void OnButtonPressSpectro()
     {
         isShowingSpectro = !isShowingSpectro;
-        if (isShowingMO == true)
+        if (isShowingMO == true || isShowingTelem == true)
         {
             isShowingMO = false;
+            isShowingTelem = false;
             MOPanel.SetActive(isShowingMO);
             spectroPanel.SetActive(isShowingSpectro);
+            telemPanel.SetActive(isShowingTelem);
         }
         else
         {
@@ -62,6 +71,30 @@ public class InformationPanelScript : MonoBehaviour
         }
 
     }
+
+    public void OnButtonPressTelem()
+    {
+        isShowingTelem = !isShowingTelem;
+        if (isShowingMO == true || isShowingSpectro == true)
+        {
+            isShowingMO = false;
+            isShowingSpectro = false;
+            MOPanel.SetActive(isShowingMO);
+            spectroPanel.SetActive(isShowingSpectro);
+            telemPanel.SetActive(isShowingTelem);
+        }
+        else
+        {
+            telemPanel.SetActive(isShowingTelem);
+        }
+
+
+    }
+
+
+
+
+
 
     public void MOButtonsColor()
     {
