abcb321b155497e1d6f98611e084c92e070c9df6
QuillJoy <jwquill81@gmail.com>
2023-03-18 23:50:50 -0700

    Fix ButtonBehaviour issues
    I simplified the ButtonBehaviour script so that it works for any of the four cards
    and uses only one function. The only object reference for the script are the FinalCards

--- Assets/ikeScripts/ButtonBehaviour.cs
+++ Assets/ikeScripts/ButtonBehaviour.cs
@@ -4,30 +4,18 @@ using UnityEngine;
 
 public class ButtonBehaviour : MonoBehaviour
 {
-    public GameObject card;
-    public GameObject elements;
-    public GameObject info;
+    public GameObject finalCard;
     private bool isShowing;
 
     void Start()
     {
         isShowing = false;
-        elements.SetActive(false);
-        info.SetActive(false);
+        finalCard.SetActive(isShowing);
     }
 
     public void OnButtonPress() 
     {
-        if (card.activeInHierarchy == true)
-            card.SetActive(false);
-        else
-            card.SetActive(true);
-    }
-
-    public void EVAOnButtonPress()
-    {
         isShowing = !isShowing;
-        elements.SetActive(isShowing);
-        info.SetActive(isShowing);
+        finalCard.SetActive(isShowing);
     }
 }
