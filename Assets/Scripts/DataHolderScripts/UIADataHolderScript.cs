15fa3438db569c630c12266e533049f5abdf088a
QuillJoy <jwquill81@gmail.com>
2023-03-21 18:33:13 -0700

    Add correct UIA booleans and adjust eva display

--- Assets/Scripts/DataHolderScripts/UIADataHolderScript.cs
+++ Assets/Scripts/DataHolderScripts/UIADataHolderScript.cs
@@ -15,15 +15,20 @@ public class UIADataHolderScript : MonoBehaviour
     void Start()
     {
         UIABooleans = new Dictionary<string, bool>();
-        UIABooleans.Add("EV1", false);
-        UIABooleans.Add("EV2", false);
         UIABooleans.Add("EMU1", false);
         UIABooleans.Add("EMU2", false);
-        UIABooleans.Add("supplySwitch", false);
-        UIABooleans.Add("wasteSwitch", false);
-        UIABooleans.Add("EMU", false);
-        UIABooleans.Add("emuPower", false);
-        UIABooleans.Add("depressPump", false);
+        UIABooleans.Add("O2 Supply Pressure 1", false);
+        UIABooleans.Add("O2 Supply Pressure 2", false);
+        UIABooleans.Add("O2 Supply Pressure Out 1", false);
+        UIABooleans.Add("O2 Supply Pressure Out 2", false);
+        UIABooleans.Add("EV1 Supply", false);
+        UIABooleans.Add("EV2 Supply", false);
+        UIABooleans.Add("EV1 Waste", false);
+        UIABooleans.Add("EV2 Waste", false);
+        UIABooleans.Add("EMU1 O2", false);
+        UIABooleans.Add("EMU2 O2", false);
+        UIABooleans.Add("O2 Vent", false);
+        UIABooleans.Add("Depress Pump", false);
 
 
     }
--- Assets/Scripts/DisplayScripts/UIAObjectScript.cs
+++ Assets/Scripts/DisplayScripts/UIAObjectScript.cs
@@ -19,15 +19,20 @@ public class UIAObjectScript : MonoBehaviour
 
             UIABooleans = FindObjectOfType<UIADataHolderScript>().GetUIABooleans();
             labelUIA.GetComponent<TextMeshProUGUI>().text = "" +
-            "EV1        " + UIABooleans["EV1"] + "\n" +
-            "EV2        " + UIABooleans["EV2"] + "\n" +
-            "EMU1       " + UIABooleans["EMU1"] + "\n" +
-            "EMU2       " + UIABooleans["EMU2"] + "\n" +
-            "Supply Switch  " + UIABooleans["supplySwitch"] + "\n" +
-            "Waste Switc    " + UIABooleans["wasteSwitch"] + "\n" +
-            "EMU        " + UIABooleans["EMU"] + "\n" +
-            "EMU Power  " + UIABooleans["emuPower"] + "\n" +
-            "Depress Pump   " + UIABooleans["depressPump"] + "\n" +
+            "EMU1        " + UIABooleans["EMU1"] + "\n" +
+            "EMU2        " + UIABooleans["EMU2"] + "\n" +
+            "O2 Supply Pressure 1" + UIABooleans["O2 Supply Pressure 1"] + "\n" +
+            "O2 Supply Pressure 2" + UIABooleans["O2 Supply Pressure 2"] + "\n" +
+            "O2 Supply Pressure Out 1  " + UIABooleans["O2 Supply Pressure Out 1"] + "\n" +
+            "O2 Supply Pressure Out 2    " + UIABooleans["O2 Supply Pressure Out 2"] + "\n" +
+            "EV1 Supply        " + UIABooleans["EV1 Supply"] + "\n" +
+            "EV2 Supply " + UIABooleans["EV2 Supply"] + "\n" +
+            "EV1 Waste   " + UIABooleans["EV1 Waste"] + "\n" +
+            "EV2 Waste   " + UIABooleans["EV2 Waste"] + "\n" +
+            "EMU1 O2        " + UIABooleans["EMU1 O2"] + "\n" +
+            "EMU2 O2        " + UIABooleans["EMU2 O2"] + "\n" +
+            "O2 Vent        " + UIABooleans["O2 Vent"] + "\n" +
+            "Depress Pump        " + UIABooleans["Depress Pump"] + "\n" +
             "";
 
     }
