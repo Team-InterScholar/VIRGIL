7564766a452617fcc878c8f6c1ac8484c17f2cd3
QuillJoy <jwquill81@gmail.com>
2023-04-28 00:00:59 -0700

    Suit card display values

--- Assets/Scripts/ConnScript.cs
+++ Assets/Scripts/ConnScript.cs
@@ -33,6 +33,29 @@ public class ConnScript : MonoBehaviour
     public TMPro.TMP_Text K2O;
     public TMPro.TMP_Text P2O3;
 
+    public TMPro.TMP_Text OxygenTimeLeftInfo;
+    public TMPro.TMP_Text PrimaryOxygenInfo;
+    public TMPro.TMP_Text PrimaryOxygenPressureInfo;
+    public TMPro.TMP_Text PrimaryOxygenTimeInfo;
+    public TMPro.TMP_Text PrimaryOxygenRateInfo;
+    public TMPro.TMP_Text SecondaryOxygenInfo;
+    public TMPro.TMP_Text SecondaryOxygenTimeInfo;
+    public TMPro.TMP_Text SecondaryOxygenPressureInfo;
+    public TMPro.TMP_Text SecondaryOxygenRateInfo;
+    public TMPro.TMP_Text H2OTimeLeftInfo;
+    public TMPro.TMP_Text LiquidPressureInfo;
+    public TMPro.TMP_Text GasPressureInfo;
+    public TMPro.TMP_Text WaterCapacityInfo;
+    public TMPro.TMP_Text BatteryTimeInfo;
+    public TMPro.TMP_Text BatteryPercentageInfo;
+    public TMPro.TMP_Text BatteryOutputInfo;
+    public TMPro.TMP_Text BatteryCapacityInfo;
+    public TMPro.TMP_Text FanTachometerInfo;
+    public TMPro.TMP_Text TemperatureInfo;
+    public TMPro.TMP_Text SuitPressureInfo;
+    public TMPro.TMP_Text SubPressureInfo;
+
+
     // Start is called before the first frame update
     async void Start()
     {
@@ -87,7 +110,10 @@ public class ConnScript : MonoBehaviour
         };
 
         tss.OnTSSTelemetryMsg += (telemMsg) => printUIA(telemMsg);
+        tss.OnTSSTelemetryMsg += (telemMsg) => printSUIT(telemMsg);
         //tss.OnTSSTelemetryMsg += (telemMsg) => printSpectr(telemMsg);
+
+
         // tss.OnOpen, OnError, and OnClose events just re-raise events from websockets.
         // Similar to OnTSSTelemetryMsg, create functions with the appropriate return type and parameters, and subscribe to them
         tss.OnOpen += () =>
@@ -137,6 +163,66 @@ public class ConnScript : MonoBehaviour
             o2Switch.text = "No UIA msg received";
             ventSwitch.text = "No UIA msg received";
             depressPumpSwitch.text = "No UIA msg received";
+        }
+    }
+
+    void printSUIT(TSS.Msgs.TSSMsg telemMsg)
+    {
+        if (telemMsg.UIA.Count > 0)
+        {
+            OxygenTimeLeftInfo.text = "" + telemMsg.EVA[0].t_oxygen;
+
+            PrimaryOxygenInfo.text = "" + telemMsg.EVA[0].ox_primary;
+            PrimaryOxygenTimeInfo.text = "" + telemMsg.EVA[0].t_oxygenPrimary;
+            PrimaryOxygenPressureInfo.text = "" + telemMsg.EVA[0].p_o2;
+            PrimaryOxygenRateInfo.text = "" + telemMsg.EVA[0].rate_o2;
+
+            SecondaryOxygenInfo.text = "" + telemMsg.EVA[0].ox_secondary;
+            SecondaryOxygenTimeInfo.text = "" + telemMsg.EVA[0].t_oxygenSec;
+            SecondaryOxygenPressureInfo.text = "" + telemMsg.EVA[0].p_sop;
+            SecondaryOxygenRateInfo.text = "" + telemMsg.EVA[0].rate_sop;
+
+            H2OTimeLeftInfo.text = "" + telemMsg.EVA[0].t_water;
+            LiquidPressureInfo.text = "" + telemMsg.EVA[0].p_h2o_l;
+            GasPressureInfo.text = "" + telemMsg.EVA[0].p_h2o_g;
+            WaterCapacityInfo.text = "" + telemMsg.EVA[0].cap_water;
+
+            BatteryTimeInfo.text = "" + telemMsg.EVA[0].t_battery;
+            BatteryPercentageInfo.text = "" + telemMsg.EVA[0].batteryPercent;
+            BatteryOutputInfo.text = "" + telemMsg.EVA[0].battery_out;
+            BatteryCapacityInfo.text = "" + telemMsg.EVA[0].cap_battery;
+
+            FanTachometerInfo.text = "" + telemMsg.EVA[0].v_fan;
+            TemperatureInfo.text = "TBD";
+            SuitPressureInfo.text = "" + telemMsg.EVA[0].p_suit;
+            SubPressureInfo.text = "" + telemMsg.EVA[0].p_sub;
+
+
+        }
+        else
+        {
+            OxygenTimeLeftInfo.text = "?";
+            PrimaryOxygenInfo.text = "?";
+            PrimaryOxygenTimeInfo.text = "?";
+            PrimaryOxygenRateInfo.text = "?";
+            PrimaryOxygenPressureInfo.text = "?";
+            SecondaryOxygenInfo.text = "?";
+            SecondaryOxygenTimeInfo.text = "?";
+            SecondaryOxygenPressureInfo.text = "?";
+            SecondaryOxygenRateInfo.text= "?";
+            H2OTimeLeftInfo.text = "?";
+            LiquidPressureInfo.text = "?";
+            GasPressureInfo.text = "?";
+            WaterCapacityInfo.text = "?";
+            BatteryTimeInfo.text= "?";
+            BatteryPercentageInfo.text = "?";
+            BatteryOutputInfo.text = "?";
+            BatteryCapacityInfo.text = "?";
+            FanTachometerInfo.text = "?";
+            TemperatureInfo.text = "?";
+            SuitPressureInfo.text = "?";
+            SubPressureInfo.text = "?";
+
         }
     }
 
